bf48b02d752e39e014f66bdb1c011ef2d4016010
David Mwendwa <klatzdavie19@gmail.com>
2021-05-14 19:30:22 +0300

    Implemented npm module, winston, for logging errors to a file system.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('express-async-errors');
+const winston = require('winston')
 const config = require('config')
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -14,6 +15,8 @@ const auth = require('./routes/auth')
 
 const express = require('express');
 const app = express();
+
+winston.add(winston.transports.File, { filename: 'logfile.log' });
 
 if (!config.get('jwtPrivateKey')) {
   console.error("FATAL ERROR: jwtPivateKey is not defined.")
