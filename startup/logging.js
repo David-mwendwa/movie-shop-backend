c86fefc97f6265757f70c42c814f0da9a6c9aaae
David Mwendwa <klatzdavie19@gmail.com>
2021-05-15 12:05:08 +0300

    Updated the aapp show unhandled exceptions on the console.

--- startup/logging.js
+++ startup/logging.js
@@ -3,15 +3,24 @@ require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function () {
-  process.on('uncaughtException', (ex) => {
-    winston.error(ex.message, ex);
-    process.exit(1);
-  });
+  winston.handleExceptions(
+    new winston.transports.Console({colorize: true, prettyPrint: true}),
+    new winston.transports.File({ filename: 'uncaughtExecptions.log' })
+  );
 
   process.on('unhandledRejection', (ex) => {
-    winston.error(ex.message, ex);
-    process.exit(1);
+    throw ex;
   });
+  
+  // process.on('uncaughtException', (ex) => {
+  //   winston.error(ex.message, ex);
+  //   process.exit(1);
+  // });
+
+  // process.on('unhandledRejection', (ex) => {
+  //   winston.error(ex.message, ex);
+  //   process.exit(1);
+  // });
 
   winston.add(winston.transports.File, { filename: 'logfile.log' });
   winston.add(winston.transports.MongoDB, {
