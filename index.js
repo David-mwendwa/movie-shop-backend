f5ee27809153e983d7376581e0740777991edd11
David Mwendwa <klatzdavie19@gmail.com>
2021-05-15 11:44:20 +0300

    Moved all the logic for error handling into logging.js, startup folder

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
 const express = require('express');
 const app = express();
+
+require('./startup/logging')
 require('./startup/routes')(app);
 require('./startup/db')();
 
-process.on('uncaughtException', (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-process.on('unhandledRejection', (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-winston.add(winston.transports.File, { filename: 'logfile.log' });
-winston.add(winston.transports.MongoDB, {
-  db: 'mongodb://localhost/vidly',
-  level: 'info',
-});
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPivateKey is not defined.');
