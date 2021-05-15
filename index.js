3796b3be61f70de45dd4a58f4b4060fcc886bd8d
David Mwendwa <klatzdavie19@gmail.com>
2021-05-15 11:37:16 +0300

    Extracted database login from index file to db module, startup folder.

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
 require('express-async-errors');
-const winston = require('winston')
-require('winston-mongodb')
-const config = require('config')
+const winston = require('winston');
+require('winston-mongodb');
+const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
-
 
 const express = require('express');
 const app = express();
-require('./startup/routes')(app)
+require('./startup/routes')(app);
+require('./startup/db')();
 
 process.on('uncaughtException', (ex) => {
   winston.error(ex.message, ex);
-  process.exit(1)
-})
+  process.exit(1);
+});
 
 process.on('unhandledRejection', (ex) => {
   winston.error(ex.message, ex);
-  process.exit(1)
+  process.exit(1);
 });
 
 winston.add(winston.transports.File, { filename: 'logfile.log' });
-winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly', level: 'info' });
+winston.add(winston.transports.MongoDB, {
+  db: 'mongodb://localhost/vidly',
+  level: 'info',
+});
 
 if (!config.get('jwtPrivateKey')) {
-  console.error("FATAL ERROR: jwtPivateKey is not defined.")
-  process.exit(1)
+  console.error('FATAL ERROR: jwtPivateKey is not defined.');
+  process.exit(1);
 }
-
-mongoose
-  .connect('mongodb://localhost/vidly')
-  .then(() => console.log('connected to mongoDB...'))
-  .catch((err) => console.error('could not connect mongoDB...', err));
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
