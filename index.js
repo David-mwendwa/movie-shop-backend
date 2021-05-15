0d939b716bf08f740603ca7c74785bfb6f540cae
David Mwendwa <klatzdavie19@gmail.com>
2021-05-15 11:29:22 +0300

    Code refactoring: Moved the routes logic into a sepatate module, within the startup folder and required it in the index file.

--- index.js
+++ index.js
@@ -6,19 +6,13 @@ const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 
-const error = require('./middleware/error')
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth')
 
 const express = require('express');
 const app = express();
+require('./startup/routes')(app)
 
 process.on('uncaughtException', (ex) => {
-  winston.error(ex.message, ex)
+  winston.error(ex.message, ex);
   process.exit(1)
 })
 
@@ -28,7 +22,7 @@ process.on('unhandledRejection', (ex) => {
 });
 
 winston.add(winston.transports.File, { filename: 'logfile.log' });
-winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost.vidly', level: 'info' });
+winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly', level: 'info' });
 
 if (!config.get('jwtPrivateKey')) {
   console.error("FATAL ERROR: jwtPivateKey is not defined.")
@@ -40,14 +34,6 @@ mongoose
   .then(() => console.log('connected to mongoDB...'))
   .catch((err) => console.error('could not connect mongoDB...', err));
 
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
